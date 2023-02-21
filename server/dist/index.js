786f188b4041ffd1d4dc564a0965fc3b47eeec0e
Steven <stevenlin3263@gmail.com>
2023-02-21 16:20:49 +0800

    build the database connection

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,13 +2,21 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello!!!!');
+require('dotenv').config();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+mongoose_1.default.set('strictQuery', false);
+mongoose_1.default.connect((_b = (_a = process.env) === null || _a === void 0 ? void 0 : _a.MONGO_URL) !== null && _b !== void 0 ? _b : '').then(() => {
+    console.log('DB Connection is successful');
+}).catch((error) => {
+    console.log(error.message);
 });
-app.listen(port, () => {
-    console.log('now listening on port');
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on Port ${process.env.PORT}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
 const app: Express = express();
-const port = 3000;
+require('dotenv').config();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello!!!!')
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log('now listening on port')
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env?.MONGO_URL ?? '').then(() => {
+  console.log('DB Connection is successful')
+}).catch((error) => {
+  console.log(error.message)
+})
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`)
 });
